a56653bda4a9ba70058d0f56112ca847051a320d
MdTanzil <mdtanzil.py@gmail.com>
2024-11-21 04:25:44 +0600

    login setup

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,11 +1,48 @@
+import axios from "axios";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 const LoginForm = () => {
+  const { setAuth } = useAuth();
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data) => {
+    try {
+      const requestData = {
+        email: data.email,
+        password: data.password,
+      };
+      const response = await axios.post(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/api/auth/login`,
+        requestData
+      );
+
+      if (response.status === 200) {
+        const { tokens, user } = response.data.data;
+
+        if (tokens) {
+          const authToken = tokens.accessToken;
+          const refreshToken = tokens.refreshToken;
+
+          console.log(`Login time auth token: ${authToken}`);
+          setAuth({ user, authToken, refreshToken });
+
+          navigate("/");
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      setError("root.random", {
+        type: "random",
+        message: `User with email ${data.email} is not found`,
+      });
+    }
+  };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-4">
@@ -42,7 +79,7 @@ const LoginForm = () => {
           <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>
         )}
       </div>
-      <div className="mb-6 flex gap-2 items-center">
+      {/* <div className="mb-6 flex gap-2 items-center">
         <input
           type="checkbox"
           {...register("admin")}
@@ -52,7 +89,8 @@ const LoginForm = () => {
         <label htmlFor="admin" className="block ">
           Login as Admin
         </label>
-      </div>
+      </div> */}
+      <p className="text-red-500">{errors?.root?.random?.message}</p>
       <button
         type="submit"
         className="w-full bg-primary text-white py-3 rounded-lg mb-4"
